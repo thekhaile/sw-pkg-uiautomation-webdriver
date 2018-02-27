b2c1911d371a2b492a389c245fb22d0eb5b8f7ac
Ningxin Liao <ningxin.liao@mutualmobile.com>
2018-02-27 17:36:21 -0600

    Work-around for MicrosoftEdge

--- components/projects.py
+++ components/projects.py
@@ -39,6 +39,9 @@ class Projects(object):
         return projectjobCount.getLabel()
 
     def selectAProject(self, rowOrder=0):
+        # This is a work-around for MicrosoftEdge not displaying the project table in the timely manner
+        if self.testCase.app.isMicrosoftEdge():
+            sleep(50)
         row = self.getAProject(rowOrder)
         # Had to go to 'th' level to proceed, instead of 'tr' b/c of Firefox issue
         el = row.find_element(self.testCase.app.getStrategy().CSS_SELECTOR, 'th')
