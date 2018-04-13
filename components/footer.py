ff6e760a89d8d8fc85c0461321ee8de13e6e7966
Ningxin Liao <ningxin.liao@mutualmobile.com>
2018-04-13 17:33:49 -0500

    initial commit for website footer scripts

---
+++ components/footer.py
@@ -0,0 +1,68 @@
+__author__ = 'ningxinliao'
+from time import sleep
+from projectBase import ProjectBase
+import string
+import random
+
+class Footer(object):
+
+    def __init__(self, testCase):
+        # type: (ProjectBase) -> None
+        """
+        :param testCase: will be the ProjectBase object when a test is run.
+        """
+        self.testCase = testCase
+
+    def getContractorSolutionsLink(self):
+        footerList = self.testCase.app.findElements(self.testCase.app.getStrategy().CSS_SELECTOR, 'a.footer-item')
+        el = footerList[0]
+        el = self.testCase.UIType.Element(el)
+        return el.getLabel()
+
+    def tapContractorSolutionsLink(self):
+        el = self.getContractorSolutionsLink()
+        el.tap()
+
+    def getLegalDisclaimer(self):
+        footerList = self.testCase.app.findElements(self.testCase.app.getStrategy().CSS_SELECTOR, 'a.footer-item')
+        el = footerList[1]
+        el = self.testCase.UIType.Element(el)
+        return el.getLabel()
+
+    def tapLegalDisclaimer(self):
+        el = self.getLegalDisclaimer()
+        el.tap()
+
+    def getContactEmail(self):
+        el = self.testCase.app.findElements(self.testCase.app.getStrategy().CSS_SELECTOR, 'span.footer-item')
+        email = el[-1]
+        email = self.testCase.UIType.Element(email)
+        return email.getLabel()
+
+    def getTwitterIcon(self):
+        container = self.testCase.app.findElement(self.testCase.app.getStrategy().CSS_SELECTOR, 'div.footer-group.icons')
+        icons = container.find_elements(self.testCase.app.getStrategy().CSS_SELECTOR, 'img')
+        el = icons[0]
+        el = self.testCase.UIType.Button(el)
+        return el.ui_object.get_attribute('alt')
+
+    def getFacebookIcon(self):
+        container = self.testCase.app.findElement(self.testCase.app.getStrategy().CSS_SELECTOR, 'div.footer-group.icons')
+        icons = container.find_elements(self.testCase.app.getStrategy().CSS_SELECTOR, 'img')
+        el = icons[1]
+        el = self.testCase.UIType.Button(el)
+        return el.ui_object.get_attribute('alt')
+
+    def getLinkedinIcon(self):
+        container = self.testCase.app.findElement(self.testCase.app.getStrategy().CSS_SELECTOR, 'div.footer-group.icons')
+        icons = container.find_elements(self.testCase.app.getStrategy().CSS_SELECTOR, 'img')
+        el = icons[2]
+        el = self.testCase.UIType.Button(el)
+        return el.ui_object.get_attribute('alt')
+
+    def getYoutubeIcon(self):
+        container = self.testCase.app.findElement(self.testCase.app.getStrategy().CSS_SELECTOR, 'div.footer-group.icons')
+        icons = container.find_elements(self.testCase.app.getStrategy().CSS_SELECTOR, 'img')
+        el = icons[2]
+        el = self.testCase.UIType.Button(el)
+        return el.ui_object.get_attribute('alt')
---
+++ tests/testWebsiteFooter.py
@@ -0,0 +1,113 @@
+__author__ = 'ningxinliao'
+import sys, os
+from time import sleep
+from projectBase import ProjectBase
+import pytest
+from southwire_pkg_uiautomation_webdriver.components.navigation import Navigation
+from southwire_pkg_uiautomation_webdriver.components.authentication import Authentication
+from southwire_pkg_uiautomation_webdriver.components.projects import Projects
+from southwire_pkg_uiautomation_webdriver.components.footer import Footer
+
+
+import unidecode
+
+class TestWebsiteFooter(ProjectBase):
+    PROJECTS_PAGE = 'https://southwire-configurator-test.firebaseapp.com/projects'
+
+    def __init__(self, *args, **kwargs):
+        super(TestWebsiteFooter, self).__init__(*args, **kwargs)
+        self.navigation = Navigation(self)
+        self.authentication = Authentication(self)
+        self.projects = Projects(self)
+        self.footer = Footer(self)
+
+    @pytest.mark.ac
+    def testContractorSolutionsLinkIsDisplayed(self):
+        email = 'ningxin.liao@mutualmobile.com'
+        password = 'newpassword'
+
+        self.caseId = 1381322
+        self.navigation.navigateToLoginPage()
+        self.authentication.login(email, password)
+        sleep(3)
+        footer = self.footer.getContractorSolutionsLink()
+
+        self.assertion.assertEqual(footer, 'ContractorSolutions.Southwire.com')
+
+    @pytest.mark.ac
+    def testLegalDisclaimerIsDisplayed(self):
+        email = 'ningxin.liao@mutualmobile.com'
+        password = 'newpassword'
+
+        self.caseId = 1381324
+        self.navigation.navigateToLoginPage()
+        self.authentication.login(email, password)
+        sleep(3)
+        footer = self.footer.getLegalDisclaimer()
+
+        self.assertion.assertEqual(footer, 'Legal Disclaimer')
+
+    @pytest.mark.ac
+    def testContactEmailIsDisplayed(self):
+        email = 'ningxin.liao@mutualmobile.com'
+        password = 'newpassword'
+
+        self.caseId = 1381326
+        self.navigation.navigateToLoginPage()
+        self.authentication.login(email, password)
+        sleep(3)
+        footer = self.footer.getContactEmail()
+
+        self.assertion.assertEqual(footer, 'ProjectConstructionTeam@Southwire.com')
+
+    @pytest.mark.ac
+    def testTwitterIconIsDisplayed(self):
+        email = 'ningxin.liao@mutualmobile.com'
+        password = 'newpassword'
+
+        self.caseId = 1381327
+        self.navigation.navigateToLoginPage()
+        self.authentication.login(email, password)
+        sleep(3)
+        footer = self.footer.getTwitterIcon()
+
+        self.assertion.assertExists(footer)
+
+    @pytest.mark.ac
+    def testFacebookIconIsDisplayed(self):
+        email = 'ningxin.liao@mutualmobile.com'
+        password = 'newpassword'
+
+        self.caseId = 138129
+        self.navigation.navigateToLoginPage()
+        self.authentication.login(email, password)
+        sleep(3)
+        footer = self.footer.getFacebookIcon()
+
+        self.assertion.assertExists(footer)
+
+    @pytest.mark.ac
+    def testLinkedinIconIsDisplayed(self):
+        email = 'ningxin.liao@mutualmobile.com'
+        password = 'newpassword'
+
+        self.caseId = 138131
+        self.navigation.navigateToLoginPage()
+        self.authentication.login(email, password)
+        sleep(3)
+        footer = self.footer.getLinkedinIcon()
+
+        self.assertion.assertExists(footer)
+
+    @pytest.mark.ac
+    def testYoutubeIconIsDisplayed(self):
+        email = 'ningxin.liao@mutualmobile.com'
+        password = 'newpassword'
+
+        self.caseId = 138133
+        self.navigation.navigateToLoginPage()
+        self.authentication.login(email, password)
+        sleep(3)
+        footer = self.footer.getYoutubeIcon()
+
+        self.assertion.assertExists(footer)
