72a87513b4ff83a93c1979d7c742ce7f7a598052
Ningxin Liao <ningxin.liao@mutualmobile.com>
2018-04-30 11:20:53 -0500

    added screenshot flow for circuit deletion

--- tests/test_ScreenshotFlow2.py
+++ tests/test_ScreenshotFlow2.py
@@ -15,6 +15,7 @@ from southwire_pkg_uiautomation_webdriver.components.jobSummary.reels import Ree
 from southwire_pkg_uiautomation_webdriver.components.requestQuote import RequestQuote
 from southwire_pkg_uiautomation_webdriver.components.registration import Registration
 from southwire_pkg_uiautomation_webdriver.components.passwordReset import PasswordReset
+from southwire_pkg_uiautomation_webdriver.components.circuit import Circuit
 import unidecode
 
 
@@ -36,6 +37,7 @@ class TestScreenshotFlow2(ProjectBase):
         self.requestQuote = RequestQuote(self)
         self.registration = Registration(self)
         self.passwordReset = PasswordReset(self)
+        self.circuit = Circuit(self)
 
 
     def saveScreenshot(self, description):
@@ -54,14 +56,17 @@ class TestScreenshotFlow2(ProjectBase):
         self.jobSummary.tapConfigureJob()
         self.saveScreenshot('flow_configurator_WithCircuitCollapsed')
         sleep(2)
+
         """Screenshot - Configurator - WIth circuit expanded"""
         self.feederSchedule.tapExpandArrow()
         self.saveScreenshot('flow_configurator_WithCircuitExpanded')
         sleep(2)
+
         """Screenshot - Create Reel - Empty"""
         self.reelList.tapCreateReel()
         self.saveScreenshot('flow_createReel_Empty')
         sleep(2)
+
         """Screenshot - Configurator - with reel list and circuit"""
         self.reel.enterRandomReelName()
         sleep(2)
@@ -69,10 +74,12 @@ class TestScreenshotFlow2(ProjectBase):
         sleep(2)
         self.saveScreenshot('flow_configurator_WithReelListAndCircuit')
         sleep(2)
+
         """Screenshot - Configurator - Circuits on Reel Empty"""
         self.feederSchedule.switchToCircuitsOnReelTab()
         self.saveScreenshot('flow_configurator_CircuitsOnReelEmpty')
         sleep(2)
+
         """Screenshot - Configurator - Circuits on Reel with circuits"""
         self.feederSchedule.switchToAvailableCircuitTab()
         sleep(2)
@@ -81,36 +88,43 @@ class TestScreenshotFlow2(ProjectBase):
         self.feederSchedule.switchToCircuitsOnReelTab()
         self.saveScreenshot('flow_configurator_CircuitsOnReelWithCircuits')
         sleep(2)
+
         """Screenshot - Job Summary - Feeder Schedule collapsed"""
         self.navigation.tapJobBreadcrumb()
         sleep(2)
         self.saveScreenshot('flow_jobSummary_FeederScheduleCollapsed')
         sleep(2)
+
         """Screenshot - Job Summary - Feeder Schedule expanded"""
         self.feederScheduleOnJobSummary.tapExpandArrow()
         sleep(2)
         self.saveScreenshot('flow_jobSummary_FeederScheduleExpanded')
         sleep(2)
+
         """Job Summary - Reel collapsed"""
         self.reels.switchToReelsTab()
         sleep(2)
         self.saveScreenshot('flow_jobSummary_ReelCollapsed')
         sleep(2)
+
         """Job Summary - Reel expanded"""
         self.reels.tapExpandArrow()
         sleep(2)
         self.saveScreenshot('flow_jobSummary_ReelExpanded')
         sleep(2)
+
         """Job summary - Circuits on reel expanded"""
         self.reels.tapExpandArrow()
         sleep(2)
         self.saveScreenshot('flow_jobSummary_CircuitOnReelExpanded')
         sleep(2)
+
         """Request for Quote - Empty"""
         self.jobSummary.tapRequestQuote()
         sleep(2)
         self.saveScreenshot('flow_requestForQuote_Empty')
         sleep(2)
+
         """Job Summary - RFQ Submited"""
         self.requestQuote.tapSubmit()
         sleep(2)
@@ -118,11 +132,13 @@ class TestScreenshotFlow2(ProjectBase):
         sleep(2)
         self.navigation.navigateToProjectsPage()
         sleep(2)
+
         """Job list - Quote sent"""
         self.projectList.selectAProject()
         sleep(2)
         self.saveScreenshot('flow_jobList_quoteSent')
         sleep(2)
+
         """Account page"""
         self.registration.tapAccount()
         sleep(2)
@@ -139,14 +155,49 @@ class TestScreenshotFlow2(ProjectBase):
         """Forgot Password - Empty"""
         self.saveScreenshot('flow_forgotPassword_Empty')
         sleep(2)
+
         """Forgot Password - Error"""
         self.passwordReset.enterEmail('ningxin.com')
         self.passwordReset.tapReset()
         sleep(2)
         self.saveScreenshot('flow_forgotPassword_Error')
         sleep(2)
+
         """Log in - Password reset email sent"""
         self.passwordReset.enterEmail(email)
         self.passwordReset.tapReset()
         sleep(2)
         self.saveScreenshot('flow_logIn_PasswordResetEmailSent')
+
+    @pytest.mark.ac
+    def testScreenshotFlowCircuitDeletion(self):
+        email = 'ningxin.liao+regression3@mutualmobile.com'
+        password = 'password'
+
+        self.navigation.navigateToLoginPage()
+        self.authentication.login(email, password)
+        self.projectList.selectAProject()
+        self.job.createAJob()
+        sleep(2)
+        self.jobList.selectAJob()
+        sleep(2)
+        self.jobSummary.tapConfigureJob()
+        sleep(2)
+        self.circuit.createSmallCircuit()
+        sleep(2)
+        """Circuit deletion - selecte a circuit"""
+        self.saveScreenshot('flow_circuitDeletion_SelecteACircuit')
+        sleep(2)
+
+        """Circuit deletion - deletion confirmation"""
+        self.feederSchedule.tapOverflow()
+        sleep(2)
+        self.feederSchedule.tapDeleteCircuit()
+        sleep(2)
+        self.saveScreenshot('flow_circuitDeletion_DeletionConfirmation')
+        sleep(2)
+
+        """Circuit deletion - after deletion"""
+        self.feederSchedule.tapConfirmDelete()
+        sleep(2)
+        self.saveScreenshot('flow_circuitDeletion_AfterDeletion')
